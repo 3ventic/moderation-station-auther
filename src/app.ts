212d5f379bfdb9196a13f55c22651ac9a69e3152
Werner Vänttinen <ventic@3v.fi>
2020-05-15 14:01:18 +0200

    formatting, fix a return type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import {
 	PugDefaults,
 	OAuth2CallbackQuery,
@@ -6,35 +6,35 @@ import {
 	OAuth2CodeExchangeResult,
 	ModLookupUserTotals,
 	HelixGetUsersResult,
-	HelixUser
-} from "./models";
-import fetch, { Response as FetchResponse } from "node-fetch";
-import Session from "express-session";
-import formurlencoded from "form-urlencoded";
-import Discord from "discord.js";
-import { Roles, setRolesAndNick, GUILD_ID, DISCORD_API_BASE } from "./bot/bot";
+	HelixUser,
+} from './models';
+import fetch, { Response as FetchResponse } from 'node-fetch';
+import Session from 'express-session';
+import formurlencoded from 'form-urlencoded';
+import Discord from 'discord.js';
+import { Roles, setRolesAndNick, GUILD_ID, DISCORD_API_BASE } from './bot/bot';
 
-const BASE_PATH: string = process.env.BASE_PATH || "";
+const BASE_PATH: string = process.env.BASE_PATH || '';
 const app: express.Express = express();
 
 const defaults: PugDefaults = {
 	discord_callback: encodeURIComponent(process.env.DISCORD_CALLBACK!),
 	discord_client_id: process.env.DISCORD_CLIENT_ID!,
-	discord_scopes: "identify%20guilds%20guilds.join",
+	discord_scopes: 'identify%20guilds%20guilds.join',
 	twitch_callback: encodeURIComponent(process.env.TWITCH_CALLBACK!),
 	twitch_client_id: process.env.TWITCH_CLIENT_ID!,
-	twitch_scopes: "",
-	base_path: BASE_PATH
+	twitch_scopes: '',
+	base_path: BASE_PATH,
 };
 
 const FOLLOWS_REQUIRED: number = (process.env.FOLLOWS_LIMIT && parseInt(process.env.FOLLOWS_LIMIT, 10)) || 15000;
 const PARTNERS_REQUIRED: number = (process.env.PARTNERS_LIMIT && parseInt(process.env.PARTNERS_LIMIT, 10)) || 1;
 
-const TWITCH_ID_BASE: string = "https://id.twitch.tv";
+const TWITCH_ID_BASE: string = 'https://id.twitch.tv';
 
 const CODE_EXCHANGE_BASE_MAP: { [key: string]: string } = {
 	twitch: TWITCH_ID_BASE,
-	discord: DISCORD_API_BASE
+	discord: DISCORD_API_BASE,
 };
 
 const exchangeCode: (
@@ -50,11 +50,11 @@ const exchangeCode: (
 ) => {
 	try {
 		const r: FetchResponse = await fetch(`${CODE_EXCHANGE_BASE_MAP[service]}/oauth2/token`, {
-			method: "POST",
+			method: 'POST',
 			body: formurlencoded(exchange, { ignorenull: false }),
 			headers: {
-				"Content-Type": "application/x-www-form-urlencoded"
-			}
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
 		});
 
 		const j: OAuth2CodeExchangeResult = await r.json();
@@ -64,7 +64,7 @@ const exchangeCode: (
 		req.session!.tokens[service] = j;
 		return true;
 	} catch (e) {
-		console.error("exchangeCode", service, e);
+		console.error('exchangeCode', service, e);
 		res.redirect(302, BASE_PATH + `/${service}_error`);
 		return false;
 	}
@@ -72,18 +72,18 @@ const exchangeCode: (
 
 const twitchUser: (session: Express.Session) => Promise<HelixUser | null> = async (session: Express.Session) => {
 	try {
-		const r: FetchResponse = await fetch("https://api.twitch.tv/helix/users", {
+		const r: FetchResponse = await fetch('https://api.twitch.tv/helix/users', {
 			headers: {
 				Authorization: `Bearer ${session.tokens.twitch.access_token}`,
-				'Client-ID': defaults.twitch_client_id
-			}
+				'Client-ID': defaults.twitch_client_id,
+			},
 		});
 
 		const j: HelixGetUsersResult = await r.json();
-		console.log("twitchUser", r.status, j);
+		console.log('twitchUser', r.status, j);
 		return j.data[0] || null;
 	} catch (e) {
-		console.error("twitchUser", e);
+		console.error('twitchUser', e);
 		return null;
 	}
 };
@@ -92,14 +92,14 @@ const modTotals: (login: string) => Promise<ModLookupUserTotals | null> = async 
 	try {
 		const r: FetchResponse = await fetch(`https://modlookup.3v.fi/api/user-totals/${encodeURIComponent(login)}`, {
 			headers: {
-				"User-Agent": "MSA/0.1.0 (Moderation Station Discord)"
-			}
+				'User-Agent': 'MSA/0.1.0 (Moderation Station Discord)',
+			},
 		});
 
 		const j: ModLookupUserTotals = await r.json();
 		return j;
 	} catch (e) {
-		console.error("twitchLogin", e);
+		console.error('twitchLogin', e);
 		return null;
 	}
 };
@@ -108,15 +108,15 @@ const discordUser: (session: Express.Session) => Promise<Discord.User | null> = 
 	try {
 		const r: FetchResponse = await fetch(`${DISCORD_API_BASE}/users/@me`, {
 			headers: {
-				Authorization: `${session.tokens.discord.token_type} ${session.tokens.discord.access_token}`
-			}
+				Authorization: `${session.tokens.discord.token_type} ${session.tokens.discord.access_token}`,
+			},
 		});
 
 		const j: Discord.User = await r.json();
-		console.log("discordUser", r.status, j);
+		console.log('discordUser', r.status, j);
 		return j;
 	} catch (e) {
-		console.error("discordUser", e);
+		console.error('discordUser', e);
 		return null;
 	}
 };
@@ -127,9 +127,9 @@ const grantRoles: (
 	isPartner: boolean,
 	nick: string
 ) => Promise<boolean> = async (session: Express.Session, isStaff: boolean, isPartner: boolean, nick: string) => {
-	console.log("grantRoles", isStaff, isPartner);
+	console.log('grantRoles', isStaff, isPartner);
 	const dUser: Discord.User | null = await discordUser(session);
-	console.log("grantRoles", dUser);
+	console.log('grantRoles', dUser);
 	if (dUser === null) {
 		return false;
 	}
@@ -144,184 +144,184 @@ const grantRoles: (
 		await setRolesAndNick(session, dUser.id, roles, nick);
 		return true;
 	} catch (e) {
-		console.error("grantRoles", e);
+		console.error('grantRoles', e);
 		return false;
 	}
 };
 
 const getNick: (tUser?: HelixUser) => string = (tUser?: HelixUser) => {
 	if (!tUser) {
-		return "UNKNOWN";
+		return 'UNKNOWN';
 	}
 	if (!tUser.display_name) {
 		return tUser.login;
 	}
-	if (tUser.display_name.toLowerCase().replace(/ /g, "") !== tUser.login) {
+	if (tUser.display_name.toLowerCase().replace(/ /g, '') !== tUser.login) {
 		return tUser.login;
 	}
 	return tUser.display_name;
 };
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
 app.use(
 	Session({
 		cookie: {
-			secure: !!process.env.SECURE_COOKIE
+			secure: !!process.env.SECURE_COOKIE,
 		},
 		secret: process.env.COOKIE_SECRET!,
 		resave: false,
-		saveUninitialized: true
+		saveUninitialized: true,
 	})
 );
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
 	req.session!.regenerate(() => {
-		res.redirect(302, BASE_PATH + "/get-started");
+		res.redirect(302, BASE_PATH + '/get-started');
 	});
 });
 
-app.get("/get-started", (req, res) => {
-	res.render("discord", { ...defaults, title: "Link Discord", session_id: encodeURIComponent(req.sessionID!) });
+app.get('/get-started', (req, res) => {
+	res.render('discord', { ...defaults, title: 'Link Discord', session_id: encodeURIComponent(req.sessionID!) });
 });
 
-app.get("/discord", (req, res) => {
-	res.render("discord", { ...defaults, title: "Link Discord", session_id: encodeURIComponent(req.sessionID!) });
+app.get('/discord', (req, res) => {
+	res.render('discord', { ...defaults, title: 'Link Discord', session_id: encodeURIComponent(req.sessionID!) });
 });
 
-app.get("/oauth2/discord", async (req, res) => {
+app.get('/oauth2/discord', async (req, res) => {
 	const query: OAuth2CallbackQuery = req.query as OAuth2CallbackQuery;
 	if (query.state !== req.sessionID) {
-		res.redirect(302, BASE_PATH + "/state_error");
+		res.redirect(302, BASE_PATH + '/state_error');
 	} else {
 		const exchange: OAuth2CodeExchange = {
 			client_id: defaults.discord_client_id,
 			client_secret: process.env.DISCORD_SECRET!,
 			code: query.code,
-			grant_type: "authorization_code",
+			grant_type: 'authorization_code',
 			redirect_uri: decodeURIComponent(defaults.discord_callback),
-			scope: decodeURIComponent(defaults.discord_scopes)
+			scope: decodeURIComponent(defaults.discord_scopes),
 		};
 
-		if (await exchangeCode(req, res, exchange, "discord")) {
-			res.redirect(302, BASE_PATH + "/twitch");
+		if (await exchangeCode(req, res, exchange, 'discord')) {
+			res.redirect(302, BASE_PATH + '/twitch');
 		}
 	}
 });
 
-app.get("/twitch", (req, res) => {
-	res.render("twitch", { ...defaults, title: "Link Twitch", session_id: encodeURIComponent(req.sessionID!) });
+app.get('/twitch', (req, res) => {
+	res.render('twitch', { ...defaults, title: 'Link Twitch', session_id: encodeURIComponent(req.sessionID!) });
 });
 
-app.get("/oauth2/twitch", async (req, res) => {
+app.get('/oauth2/twitch', async (req, res) => {
 	const query: OAuth2CallbackQuery = req.query as OAuth2CallbackQuery;
 	if (query.state !== req.sessionID) {
-		res.redirect(302, BASE_PATH + "/state_error");
+		res.redirect(302, BASE_PATH + '/state_error');
 	} else {
 		const exchange: OAuth2CodeExchange = {
 			client_id: defaults.twitch_client_id,
 			client_secret: process.env.TWITCH_SECRET!,
 			code: query.code,
-			grant_type: "authorization_code",
+			grant_type: 'authorization_code',
 			redirect_uri: decodeURIComponent(defaults.twitch_callback),
-			scope: decodeURIComponent(defaults.twitch_scopes)
+			scope: decodeURIComponent(defaults.twitch_scopes),
 		};
 
-		if (await exchangeCode(req, res, exchange, "twitch")) {
+		if (await exchangeCode(req, res, exchange, 'twitch')) {
 			const tUser: HelixUser | null = await twitchUser(req.session!);
 			req.session!.tUser = tUser;
 			if (tUser === null) {
-				res.redirect(302, BASE_PATH + "/twitch_error");
-			} else if (tUser.type === "staff" || tUser.broadcaster_type === "partner") {
+				res.redirect(302, BASE_PATH + '/twitch_error');
+			} else if (tUser.type === 'staff' || tUser.broadcaster_type === 'partner') {
 				if (
 					await grantRoles(
 						req.session!,
-						tUser.type === "staff",
-						tUser.broadcaster_type === "partner",
+						tUser.type === 'staff',
+						tUser.broadcaster_type === 'partner',
 						getNick(tUser)
 					)
 				) {
-					res.redirect(302, BASE_PATH + "/success");
+					res.redirect(302, BASE_PATH + '/success');
 				} else {
-					res.redirect(302, BASE_PATH + "/discord_error");
+					res.redirect(302, BASE_PATH + '/discord_error');
 				}
 			} else {
 				const mt: ModLookupUserTotals | null = await modTotals(tUser.login);
 				req.session!.mt = mt;
 				if (mt === null) {
-					res.render("ml_error", {
+					res.render('ml_error', {
 						...defaults,
-						title: "Modlookup Error",
-						session_id: encodeURIComponent(req.sessionID!)
+						title: 'Modlookup Error',
+						session_id: encodeURIComponent(req.sessionID!),
 					});
 				} else if (mt.follows >= FOLLOWS_REQUIRED && mt.partners >= PARTNERS_REQUIRED) {
 					if (
 						await grantRoles(
 							req.session!,
-							tUser.type === "staff",
-							tUser.broadcaster_type === "partner",
+							tUser.type === 'staff',
+							tUser.broadcaster_type === 'partner',
 							getNick(tUser)
 						)
 					) {
-						res.redirect(302, BASE_PATH + "/success");
+						res.redirect(302, BASE_PATH + '/success');
 					} else {
-						res.redirect(302, BASE_PATH + "/discord_error");
+						res.redirect(302, BASE_PATH + '/discord_error');
 					}
 				} else {
-					res.redirect(302, BASE_PATH + "/unfortunate");
+					res.redirect(302, BASE_PATH + '/unfortunate');
 				}
 			}
 		}
 	}
 });
 
-app.get("/state_error", (req, res) => {
-	res.render("state_error", {
+app.get('/state_error', (req, res) => {
+	res.render('state_error', {
 		...defaults,
-		title: "Cookie Error",
-		session_id: encodeURIComponent(req.sessionID!)
+		title: 'Cookie Error',
+		session_id: encodeURIComponent(req.sessionID!),
 	});
 });
 
-app.get("/twitch_error", (req, res) => {
-	res.render("twitch_error", {
+app.get('/twitch_error', (req, res) => {
+	res.render('twitch_error', {
 		...defaults,
-		title: "Twitch Error",
-		session_id: encodeURIComponent(req.sessionID!)
+		title: 'Twitch Error',
+		session_id: encodeURIComponent(req.sessionID!),
 	});
 });
 
-app.get("/discord_error", (req, res) => {
-	res.render("discord_error", {
+app.get('/discord_error', (req, res) => {
+	res.render('discord_error', {
 		...defaults,
-		title: "Discord Error",
-		session_id: encodeURIComponent(req.sessionID!)
+		title: 'Discord Error',
+		session_id: encodeURIComponent(req.sessionID!),
 	});
 });
 
-app.get("/success", (req, res) => {
-	res.render("success", {
+app.get('/success', (req, res) => {
+	res.render('success', {
 		...defaults,
-		title: "Success",
+		title: 'Success',
 		session_id: encodeURIComponent(req.sessionID!),
 		guild_id: GUILD_ID,
-		display_name: getNick(req.session!.tUser)
+		display_name: getNick(req.session!.tUser),
 	});
 });
 
-app.get("/unfortunate", (req, res) => {
-	res.render("unfortunate", {
+app.get('/unfortunate', (req, res) => {
+	res.render('unfortunate', {
 		...defaults,
-		title: "Too bad",
+		title: 'Too bad',
 		session_id: encodeURIComponent(req.sessionID!),
 		follows: req.session!.mt && req.session!.mt.follows,
 		partners: req.session!.mt && req.session!.mt.partners,
 		follows_required: FOLLOWS_REQUIRED,
-		partners_required: PARTNERS_REQUIRED
+		partners_required: PARTNERS_REQUIRED,
 	});
 });
 
-app.listen(process.env.PORT || 8080, () => console.log("listening"));
+app.listen(process.env.PORT || 8080, () => console.log('listening'));
--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,13 +1,13 @@
-import Discord from "discord.js";
-import fetch, { Response as FetchResponse } from "node-fetch";
+import Discord from 'discord.js';
+import fetch from 'node-fetch';
 
-export const GUILD_ID: string = process.env.DISCORD_GUILD_ID || "352896412880470017";
-export const DISCORD_API_BASE: string = "https://discordapp.com/api/v6";
+export const GUILD_ID: string = process.env.DISCORD_GUILD_ID || '352896412880470017';
+export const DISCORD_API_BASE: string = 'https://discordapp.com/api/v6';
 
 export enum Roles {
-	Base = "Verified",
-	Partner = "Twitch Partner",
-	Staff = "Twitch Staff"
+	Base = 'Verified',
+	Partner = 'Twitch Partner',
+	Staff = 'Twitch Staff',
 }
 
 const allRoles: Roles[] = [Roles.Base, Roles.Partner, Roles.Staff];
@@ -20,88 +20,93 @@ const notifyHook: (
 ) => void = async (member: Discord.GuildMember, removedRoles: Roles[], addedRoles: Roles[], nick: string) => {
 	if (process.env.DISCORD_HOOK) {
 		const payload: Discord.WebhookMessageOptions = {
-			username: "Gatekeeper",
+			username: 'Gatekeeper',
 			embeds: [
 				{
 					fields: [
 						{
 							inline: true,
-							name: "User",
-							value: `<@${member.id}> (${member.user.tag})`
+							name: 'User',
+							value: `<@${member.id}> (${member.user.tag})`,
 						},
 						{
 							inline: true,
-							name: "Name",
-							value: nick
+							name: 'Name',
+							value: nick,
 						},
 						{
 							inline: true,
-							name: "Joined",
-							value: member.joinedAt
-								.toISOString()
-								.replace("T", " ")
-								.replace(/\.\d+/, "")
+							name: 'Joined',
+							value: member.joinedAt.toISOString().replace('T', ' ').replace(/\.\d+/, ''),
 						},
 						{
 							inline: false,
-							name: "Removed Roles",
-							value: "-" + removedRoles.join("\n-")
+							name: 'Removed Roles',
+							value: '-' + removedRoles.join('\n-'),
 						},
 						{
 							inline: false,
-							name: "Added Roles",
-							value: "+" + addedRoles.join("\n+")
-						}
-					]
-				}
-			]
+							name: 'Added Roles',
+							value: '+' + addedRoles.join('\n+'),
+						},
+					],
+				},
+			],
 		};
 		await fetch(process.env.DISCORD_HOOK, {
-			method: "POST",
+			method: 'POST',
 			headers: {
-				"Content-Type": "application/json"
+				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify(payload)
+			body: JSON.stringify(payload),
 		});
 	}
 };
 
 const client: Discord.Client = new Discord.Client({
-	retryLimit: 5
+	retryLimit: 5,
 });
 
-client.on("error", e => {
-	console.error("discord.js", e);
+client.on('error', (e) => {
+	console.error('discord.js', e);
 });
 
-client.on("ready", () => {
+client.on('ready', () => {
 	console.log(`bot connected ${client.user.tag}`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
-export const setRolesAndNick: (session: Express.Session, userID: string, roles: Roles[], nick: string) => void = async (
+export const setRolesAndNick: (
 	session: Express.Session,
 	userID: string,
 	roles: Roles[],
 	nick: string
-) => {
+) => Promise<void> = async (session: Express.Session, userID: string, roles: Roles[], nick: string) => {
 	const guild: Discord.Guild = client.guilds.get(GUILD_ID)!;
 	const member: Discord.GuildMember = await guild.addMember(userID, {
 		accessToken: session.tokens.discord.access_token,
 		nick: nick,
 		deaf: false,
-		mute: false
+		mute: false,
 	});
-	const newRoles: Discord.Role[] = roles.map(r => guild.roles.filter(r2 => r2.name === r).first());
-	const rolesToAdd: Discord.Role[] = newRoles.filter(r => !member.roles.get(r.id));
+	const newRoles: Discord.Role[] = roles.map((r) => guild.roles.filter((r2) => r2.name === r).first());
+	const rolesToAdd: Discord.Role[] = newRoles.filter((r) => !member.roles.get(r.id));
 	const rolesToRemove: Discord.Role[] = allRoles
-		.filter(r => !newRoles.map(r => r.name).includes(r))
-		.map(r => guild.roles.filter(r2 => r2.name === r).first())
-		.filter(r => !!member.roles.get(r.id));
-	console.log("setRolesAndNick", nick, userID, "+", rolesToAdd.map(r => r.name), "-", rolesToRemove.map(r => r.name));
+		.filter((r) => !newRoles.map((r) => r.name).includes(r))
+		.map((r) => guild.roles.filter((r2) => r2.name === r).first())
+		.filter((r) => !!member.roles.get(r.id));
+	console.log(
+		'setRolesAndNick',
+		nick,
+		userID,
+		'+',
+		rolesToAdd.map((r) => r.name),
+		'-',
+		rolesToRemove.map((r) => r.name)
+	);
 	if (member.nickname !== nick) {
-		await member.setNickname(nick, "Twitch display name");
+		await member.setNickname(nick, 'Twitch display name');
 	}
 	if (rolesToAdd.length > 0) {
 		await member.addRoles(rolesToAdd);
@@ -109,6 +114,11 @@ export const setRolesAndNick: (session: Express.Session, userID: string, roles: 
 	if (rolesToRemove.length > 0) {
 		await member.removeRoles(rolesToRemove);
 	}
-	notifyHook(member, rolesToRemove.map(r => r.name as Roles), rolesToAdd.map(r => r.name as Roles), nick);
+	notifyHook(
+		member,
+		rolesToRemove.map((r) => r.name as Roles),
+		rolesToAdd.map((r) => r.name as Roles),
+		nick
+	);
 	return;
 };
