699873da5f3ad63fa476972c3d7662e12f7b0965
3v <ventic@3v.fi>
2021-04-17 20:00:31 +0200

    prettify

--- public/discord.js
+++ public/discord.js
@@ -1,34 +1,36 @@
-(function() {
-	const getStarted = document.getElementById("discord");
-	const contactUs = document.getElementById("contact");
-	const hideClass = "hidden";
+(function () {
+  const getStarted = document.getElementById("discord");
+  const contactUs = document.getElementById("contact");
+  const hideClass = "hidden";
 
-	function handleRadioEvent(e) {
-		handleRadio(e.target);
-	}
+  function handleRadioEvent(e) {
+    handleRadio(e.target);
+  }
 
-	function handleRadio(e) {
-		if (e.checked) {
-			switch (e.value) {
-				case "contact":
-					if (!getStarted.classList.contains(hideClass)) {
-						getStarted.classList.add(hideClass);
-					}
-					contactUs.classList.remove(hideClass);
-					break;
-				default:
-					if (!contactUs.classList.contains(hideClass)) {
-						contactUs.classList.add(hideClass);
-					}
-					getStarted.classList.remove(hideClass);
-					break;
-			}
-		}
-	}
+  function handleRadio(e) {
+    if (e.checked) {
+      switch (e.value) {
+        case "contact":
+          if (!getStarted.classList.contains(hideClass)) {
+            getStarted.classList.add(hideClass);
+          }
+          contactUs.classList.remove(hideClass);
+          break;
+        default:
+          if (!contactUs.classList.contains(hideClass)) {
+            contactUs.classList.add(hideClass);
+          }
+          getStarted.classList.remove(hideClass);
+          break;
+      }
+    }
+  }
 
-	const radios = Array.from(document.querySelectorAll("input[name=applicant-type]"));
-	radios.forEach(r => {
-		r.onchange = handleRadioEvent;
-		handleRadio(r);
-	});
+  const radios = Array.from(
+    document.querySelectorAll("input[name=applicant-type]")
+  );
+  radios.forEach((r) => {
+    r.onchange = handleRadioEvent;
+    handleRadio(r);
+  });
 })();
